11a2b6f331c44808a9302d75b82a32e5e7fc91bd
Serdj2846 <149586019+Serdj2846@users.noreply.github.com>
2023-12-03 15:36:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ function toggleModalSize() {
     // Если модальное окно увеличено, устанавливаем размеры 50%
     modal.style.width = '50%';
     modal.style.height = '50%';
+
+    button.style.width = '60px'; // Измените размеры по вашему усмотрению
+    button.style.height = '60px';
   } else {
     // Если модальное окно уменьшено или в исходном состоянии, устанавливаем размеры 100%
     modal.style.width = '100%';
     modal.style.height = '100%';
+
+    button.style.width = '120px'; // Измените размеры по вашему усмотрению
+    button.style.height = '120px';
   }
 
   // Инвертируем флаг состояния
