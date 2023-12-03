5d2d69806ae2c41344c635436df7a65f2c8f42a2
Serdj2846 <149586019+Serdj2846@users.noreply.github.com>
2023-12-03 14:48:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Получаем ссылку на модальное окно
+var modal = document.querySelector('.modal');
+
+// Получаем ссылку на кнопку
+var button = document.querySelector('.restart');
+
+// Функция для изменения размеров модального окна
+function resizeModal() {
+  // Здесь вы можете устанавливать новые размеры для модального окна
+  modal.style.width = '50%';
+  modal.style.height = '50%';
+}
+
+// Обработчик события клика на кнопке
+button.addEventListener('click', resizeModal);
