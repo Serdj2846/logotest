47a79b62bcabf7e31a69b06c426a3888c99cc591
Serdj2846 <149586019+Serdj2846@users.noreply.github.com>
2023-12-03 16:29:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function toggleModalSize() {
   if (isModalExpanded) {
     // Если модальное окно увеличено, устанавливаем размеры 50%
     modal.style.width = '50%';
-    modal.style.height = '50%';
+    modal.style.height = '70%';
 
     button.style.width = '60px'; // Измените размеры по вашему усмотрению
-    button.style.height = '80px';
+    button.style.height = '60px';
   } else {
     // Если модальное окно уменьшено или в исходном состоянии, устанавливаем размеры 100%
     modal.style.width = '100%';
