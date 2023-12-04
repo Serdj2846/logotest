300be7652eca56566b0023d186be380be9d6a27d
Serdj2846 <149586019+Serdj2846@users.noreply.github.com>
2023-12-04 19:27:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,28 @@ function toggleModalSize() {
     // Если модальное окно увеличено, устанавливаем размеры 50%
     modal.style.width = '50%';
     modal.style.height = '50%';
-
     button.style.width = '60px';
     button.style.height = '60px';
+    
   } else {
     // Если модальное окно уменьшено или в исходном состоянии, устанавливаем размеры 100%
-    modal.style.width = '100%';
-    modal.style.height = '100%';
-
-    button.style.width = '120px';
-    button.style.height = '120px';
+    // modal.style.width = '100%';
+    // modal.style.height = '100%';
+    // button.style.width = '120px';
+    // button.style.height = '120px';
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { // для Firefox
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { // для Chrome, Safari и Opera
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // для Internet Explorer
+      document.msExitFullscreen();
+    }
   }
-
   // Инвертируем флаг состояния
   isModalExpanded = !isModalExpanded;
 }
-
 button.addEventListener('click', toggleModalSize);
 
 document.addEventListener("DOMContentLoaded", function() {
