59d887c903b46b1a66b8b6d194fe3cad5e09efcd
Serdj2846 <149586019+Serdj2846@users.noreply.github.com>
2023-12-06 18:45:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,28 +5,11 @@ var isModalExpanded = false;
 function toggleModalSize() {
   if (!isModalExpanded) {
     // Если модальное окно не увеличено, запросите развертывание на весь экран
-    if (modal.requestFullscreen) {
-      modal.requestFullscreen();
-    } else if (modal.mozRequestFullScreen) { // для Firefox
-      modal.mozRequestFullScreen();
-    } else if (modal.webkitRequestFullscreen) { // для Chrome, Safari и Opera
-      modal.webkitRequestFullscreen();
-    } else if (modal.msRequestFullscreen) { // для Internet Explorer
-      modal.msRequestFullscreen();
-    }
+    modal.style.width = '50%';
+    modal.style.height = '50%';
     button.style.width = '120px';
     button.style.height = '120px';
   } else {
-    // Если модальное окно увеличено, выходите из полноэкранного режима
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) { // для Firefox
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) { // для Chrome, Safari и Opera
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) { // для Internet Explorer
-      document.msExitFullscreen();
-    }
     modal.style.width = '50%';
     modal.style.height = '50%';
     button.style.width = '60px';
