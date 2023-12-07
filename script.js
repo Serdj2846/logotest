dc2fbb884743bf8feafb19099923e1398034d303
Serdj2846 <149586019+Serdj2846@users.noreply.github.com>
2023-12-07 17:17:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-var game = document.querySelector('.game-container');
-var button = document.querySelector('.full-screen');
-var isModalExpanded = false;
-
-function toggleModalSize() {
-  if (!isModalExpanded) {
-    game.style.width = '100%';
-    game.style.height = '100%';
-    button.style.width = '60px';
-    button.style.height = '60px';
-  } else {
-    game.style.width = '50%';
-    game.style.height = '50%';
-    button.style.width = '60px';
-    button.style.height = '60px';
-  }
-  // Инвертируем флаг состояния
-  isModalExpanded = !isModalExpanded;
-}
-button.addEventListener('click', toggleModalSize);
-
-
 document.addEventListener("DOMContentLoaded", function() {
-  // Вращаем индикатор в течение 2 секунд
+  const game = document.querySelector('.game-container');
+  const button = document.querySelector('.full-screen');
   const loader = document.querySelector(".loader");
+
+  // Проверяем существование элементов перед использованием
+  if (!game || !button || !loader) {
+    console.error("One or more elements not found.");
+    return;
+  }
+
+  let isModalExpanded = false;
+
+  function toggleModalSize() {
+    if (!isModalExpanded) {
+      game.style.width = '100%';
+      game.style.height = '100%';
+      button.style.width = '60px';
+      button.style.height = '60px';
+    } else {
+      game.style.width = '50%';
+      game.style.height = '50%';
+      button.style.width = '60px';
+      button.style.height = '60px';
+    }
+    // Инвертируем флаг состояния
+    isModalExpanded = !isModalExpanded;
+  }
+
+  button.addEventListener('click', toggleModalSize);
+
+  // Вращаем индикатор в течение 2 секунд
   let rotation = 0;
   const rotationInterval = setInterval(function() {
     rotation += 10;
